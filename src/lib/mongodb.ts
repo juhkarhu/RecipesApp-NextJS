a694621f0876109bf6a9afb49c046e702323e3a8
Juhana Karhunen <juhana.karhunen@gmail.com>
2024-10-01 12:31:50 +0300

    a

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,36 +1,28 @@
 import mongoose from 'mongoose';
-
-const MONGODB_URI = process.env.MONGODB_URI!; // Ensure this is defined in your .env file
-
+const MONGODB_URI = process.env.MONGODB_URI!; // Make sure your .env file has MONGODB_URI
+console.log(`MONGODB_URI: ${MONGODB_URI}`);
 if (!MONGODB_URI) {
   throw new Error(
     'Please define the MONGODB_URI environment variable in .env.local',
   );
 }
-
-// Use a module-level cache instead of a global one
-const cached = {
-  conn: null as mongoose.Mongoose | null,
-  promise: null as Promise<mongoose.Mongoose> | null,
-};
-
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 async function connectToDatabase() {
   if (cached.conn) {
     return cached.conn;
   }
-
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
     };
-
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
-
   cached.conn = await cached.promise;
   return cached.conn;
 }
-
 export default connectToDatabase;
